b493434f6b7135f198c390b616e73af20f1f6082
Nicholas Verrico <nicholas.verrico@icloud.com>
2021-09-21 12:51:43 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nicholas Verrico on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
